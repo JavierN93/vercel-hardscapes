64c8c7cd088b5f5135b68f87aad82c31a4ffdcde
dark-souler <ymonovy99@gmail.com>
2021-02-04 05:41:23 -0800

    issue #179 fix: remove original file name

--- src/common/utils/string.util.ts
+++ src/common/utils/string.util.ts
@@ -52,6 +52,6 @@ export function splitName(fullName: string): [string, string] {
 }
 
 export function getFileExtension(filename: string): string {
-    const lastIndex = filename.lastIndexOf('.');
-    return lastIndex < 1 ? '' : filename.substr(lastIndex);
+  const lastIndex = filename.lastIndexOf('.');
+  return lastIndex < 1 ? '' : filename.substr(lastIndex);
 }
